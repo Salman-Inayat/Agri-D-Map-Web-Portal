068404a1bdaada26d0a6213dafc17a2dc9533a37
Salman-Inayat <salmaninayat1999@gmail.com>
2021-08-02 11:18:08 +0500

    Dashboard component updated

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -11,15 +11,35 @@ import { useTheme } from "@material-ui/styles";
 
 // styles
 import useStyles from "./styles";
+import Image_Picker from "../../components/Image_Picker/Image_Picker";
+import Audio_Player from "../../components/Audio_Player/Audio_Player";
+import Paper from "@material-ui/core/Paper";
 
 export default function Dashboard(props) {
   var classes = useStyles();
-  var theme = useTheme();
 
   return (
     <>
       <Grid container spacing={4}>
-        <Grid item lg={3} md={4} sm={6} xs={12}></Grid>
+        <Grid md={12} className={classes.image_picker_grid}>
+          <Image_Picker />
+        </Grid>
+        <Grid
+          container
+          spacing={3}
+          xs="auto"
+          className={classes.output_container}
+        >
+          <Grid item md={5} sm={12}>
+            <Paper className={classes.paper}>
+              Visual result displayed here
+            </Paper>
+          </Grid>
+          <Grid item md={5} sm={12} m={2} p={3}>
+            <Audio_Player />
+          </Grid>
+        </Grid>
+        <Grid item md={12}></Grid>
       </Grid>
     </>
   );
