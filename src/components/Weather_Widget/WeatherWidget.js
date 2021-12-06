2878504541389ec04a0269fcad16d435ca0cbb89
Salman Inayat <salmaninayat1999@gmail.com>
2021-12-06 12:05:08 +0500

    Weather widget updated

--- src/components/Weather_Widget/WeatherWidget.js
+++ src/components/Weather_Widget/WeatherWidget.js
@@ -2,14 +2,13 @@ import React, { useState, useEffect } from "react";
 
 import ReactWeather from "react-open-weather-widget";
 import "react-open-weather-widget/lib/css/ReactWeather.css";
-import Geocode from "react-geocode";
 
-Geocode.setApiKey("AIzaSyAWa7-RTKOR7BulmJ1PWmDaJ9r2ZB8UqAs");
+// mapbox access token
+var ACCESS_TOKEN =
+  "pk.eyJ1Ijoic2FsbWFuLWluYXlhdCIsImEiOiJja3U3OGNzZzQzNHVlMm9xaG9sZmtoOXI3In0.rF7GhHsrNL8YPMUCLCI92A";
 
-Geocode.setLocationType("ROOFTOP");
-
-// Enable or disable logs. Its optional.
-Geocode.enableDebug();
+// LocationIQ access token
+var access_token = "pk.e83540a57ec2bee941bf85a4d0a2186c";
 
 export default function WeatherWidget(props) {
   const customStyles = {
@@ -32,19 +31,49 @@ export default function WeatherWidget(props) {
     forecastIconColor: "#4BC4F7",
   };
 
+  const [city, setCity] = useState();
+  const [status, setStatus] = useState(null);
+
+  const reverseGeocoding = function (latitude, longitude) {
+    var settings = {
+      async: true,
+      crossDomain: true,
+      url: `https://us1.locationiq.com/v1/reverse.php?key=${access_token}&lat=${latitude}&lon=${longitude}&format=json`,
+      method: "GET",
+    };
+
+    fetch(settings.url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCity(data.address.city);
+      });
+  };
+
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      console.log("Available");
+    } else {
+      console.log("Not Available");
+    }
+    setStatus("Fetching location...");
+    navigator.geolocation.getCurrentPosition((position) => {
+      reverseGeocoding(position.coords.latitude, position.coords.longitude);
+    });
+  }, []);
+
   return (
     <div>
-      {props.city ? (
+      {city ? (
         <ReactWeather
           forecast="5days"
           apikey="f6973195b3a29969a6ad7e393d2ac38b"
           type="city"
-          city={props.city}
+          city={city}
           theme={customStyles}
         />
       ) : (
         <div>
-          <h4>{""}</h4>
+          <h4>{status}</h4>
         </div>
       )}
     </div>
