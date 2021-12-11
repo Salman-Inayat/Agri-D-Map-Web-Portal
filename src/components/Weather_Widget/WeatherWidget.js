23ab76f46b762c24cc8f014568c4c2aa72376d71
Salman Inayat <salmaninayat1999@gmail.com>
2021-12-11 22:25:47 +0500

    weather widget modified

--- src/components/Weather_Widget/WeatherWidget.js
+++ src/components/Weather_Widget/WeatherWidget.js
@@ -10,6 +10,9 @@ var ACCESS_TOKEN =
 // LocationIQ access token
 var access_token = "pk.e83540a57ec2bee941bf85a4d0a2186c";
 
+// geolocation-db access token
+var token = "8dd79c70-0801-11ec-a29f-e381a788c2c0";
+
 export default function WeatherWidget(props) {
   const customStyles = {
     fontFamily: "Helvetica, sans-serif",
@@ -35,28 +38,43 @@ export default function WeatherWidget(props) {
   const [status, setStatus] = useState(null);
 
   const reverseGeocoding = function (latitude, longitude) {
-    var settings = {
-      async: true,
-      crossDomain: true,
-      url: `https://us1.locationiq.com/v1/reverse.php?key=${access_token}&lat=${latitude}&lon=${longitude}&format=json`,
-      method: "GET",
-    };
-
-    fetch(settings.url)
+    var url = `https://us1.locationiq.com/v1/reverse.php?key=${access_token}&lat=${latitude}&lon=${longitude}&format=json`;
+    fetch(url)
       .then((response) => response.json())
       .then((data) => {
         setCity(data.address.city);
+        console.log(data.address.city);
       });
   };
 
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  var requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  const getData = async () => {
+    const res = await fetch("/json/");
+    console.log(res);
+    //  setIP(res.data.IPv4);
+  };
+
   useEffect(() => {
-    if ("geolocation" in navigator) {
-      console.log("Available");
-    } else {
-      console.log("Not Available");
-    }
+    // var url = `https://cors-anywhere.herokuapp.com/https://geolocation-db.com/json/${token}`;
+
     setStatus("Fetching location...");
+    // getData();
+    // fetch(`/json/${token}/111.68.97.206`)
+    //   .then((response) => response.text())
+    //   .then((result) => console.log(result))
+    //   .catch((error) => console.log("error", error));
+
     navigator.geolocation.getCurrentPosition((position) => {
+      console.log(position.coords.latitude, position.coords.longitude);
+
       reverseGeocoding(position.coords.latitude, position.coords.longitude);
     });
   }, []);
