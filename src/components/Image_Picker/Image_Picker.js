15ee8989cc75892213cb99f4b990bbe4a0abb2a2
Salman Inayat <salmaninayat1999@gmail.com>
2021-12-11 22:25:15 +0500

    image picker updated

--- src/components/Image_Picker/Image_Picker.js
+++ src/components/Image_Picker/Image_Picker.js
@@ -2,67 +2,32 @@ import React, { useState } from "react";
 import Dropzone from "react-dropzone-uploader";
 import useStyles from "./styles.js";
 import "react-dropzone-uploader/dist/styles.css";
+import ImageUploader from "react-images-upload";
 
 function ImagePicker(props) {
-  const [imageFile, setimageFile] = useState("");
-  const [loading, setLoading] = useState();
-
   const classes = useStyles();
 
-  const getUploadParams = ({ file }) => {
-    setLoading(true);
-    setimageFile("");
-    const body = new FormData();
-    body.append("dataFiles", file);
-    return { url: props.url, body };
-  };
-
-  const handleChangeStatus = ({ xhr }) => {
-    if (xhr) {
-      xhr.onreadystatechange = () => {
-        if (xhr.readyState === 4) {
-          const result = JSON.parse(xhr.response);
-          console.log(result);
-          const new_image_file = result.filename.slice(0, -3) + "png";
-          setimageFile(new_image_file);
-          setLoading(false);
-        }
-      };
+  const onDrop = (pictureFile, pictureDataURL) => {
+    if (pictureDataURL.length > 0) {
+      props.handleImage(pictureDataURL[0]);
+    } else {
+      console.log("No image selected");
+      props.handleImagePresent(false);
     }
   };
 
   return (
     <div className={classes.image_picker_container}>
-      <Dropzone
-        getUploadParams={getUploadParams}
-        onChangeStatus={handleChangeStatus}
-        accept="image/*"
-        maxFiles={1}
-        multiple={false}
-        canCancel={false}
-        inputContent="Drop A File"
-        styles={{
-          dropzone: {
-            width: 400,
-            height: 200,
-            marginRight: 30,
-            border: "3px dashed black",
-          },
-          dropzoneActive: { borderColor: "green" },
-        }}
+      <ImageUploader
+        singleImage={true}
+        withPreview={true}
+        withIcon={true}
+        buttonText="Choose an image"
+        onChange={onDrop}
+        imgExtension={[".jpg", ".png"]}
+        label="Max file size: 10MB, accepted: JPG, PNG"
+        // maxFileSize={10242880}
       />
-      <div className={classes.image_container}>
-        {loading && (
-          <img src="/loading.gif" className={classes.loading_gif} alt="" />
-        )}
-        {imageFile && (
-          <img
-            src={`http://localhost:5000/${imageFile}`}
-            className={classes.preview_image}
-            alt=""
-          />
-        )}
-      </div>
     </div>
   );
 }
