7aef8c724f412a879406cf2ac5d244b385cd20f4
Salman-Inayat <salmaninayat1999@gmail.com>
2022-02-13 15:44:27 +0500

    mongodb cluster added

--- AgriDMap_Server/app.js
+++ AgriDMap_Server/app.js
@@ -5,12 +5,21 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 var indexRouter = require("./routes/index");
 
 var app = express();
 
 app.use(cors());
+
+mongoose.connect(
+  "mongodb+srv://demouser:demo1234@cluster0.qsdua.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+);
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
